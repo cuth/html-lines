a67c4d6583e847b01db6d5e5ec5f684e2f36f2d6
Jon Cuthbert <jon@cuth.net>
2015-02-12 00:52:53 -0500

    add logo

---
+++ js/site.js
@@ -0,0 +1,333 @@
+/*global LINES */
+
+(function () {
+    'use strict';
+
+    var logo = document.querySelector('.Logo');
+
+    var html = {
+        h: logo.querySelector('.html.h'),
+        t: logo.querySelector('.html.t'),
+        m: logo.querySelector('.html.m'),
+        l: logo.querySelector('.html.l')
+    };
+
+    var lines = {
+        l: logo.querySelector('.lines.l'),
+        i: logo.querySelector('.lines.i'),
+        n: logo.querySelector('.lines.n'),
+        e: logo.querySelector('.lines.e'),
+        s: logo.querySelector('.lines.s')
+    };
+
+    var _lines = [];
+
+    var settings = {
+        name: 'logo',
+        state: 'hidden',
+        stroke: 5
+    };
+
+    // H
+    _lines.push(LINES.createLine(
+        LINES.createAnchor({
+            el: html.h,
+            xOrigin: 'left',
+            yOrigin: 'top'
+        }),
+        LINES.createAnchor({
+            el: html.h,
+            xOrigin: 'left',
+            yOrigin: 'bottom'
+        }),
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        LINES.createAnchor({
+            el: html.h,
+            xOrigin: 'left',
+            yOrigin: 'center'
+        }),
+        LINES.createAnchor({
+            el: html.h,
+            xOrigin: 'right',
+            yOrigin: 'center'
+        }),
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        LINES.createAnchor({
+            el: html.h,
+            xOrigin: 'right',
+            yOrigin: 'top'
+        }),
+        LINES.createAnchor({
+            el: html.h,
+            xOrigin: 'right',
+            yOrigin: 'bottom'
+        }),
+        settings
+    ));
+
+    // T
+    _lines.push(LINES.createLine(
+        LINES.createAnchor({
+            el: html.t,
+            xOrigin: 'left',
+            yOrigin: 'top'
+        }),
+        LINES.createAnchor({
+            el: html.t,
+            xOrigin: 'right',
+            yOrigin: 'top'
+        }),
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        LINES.createAnchor({
+            el: html.t,
+            xOrigin: 'center',
+            yOrigin: 'top'
+        }),
+        LINES.createAnchor({
+            el: html.t,
+            xOrigin: 'center',
+            yOrigin: 'bottom'
+        }),
+        settings
+    ));
+
+    // M
+    var mTopLeftAnchor = LINES.createAnchor({
+        el: html.m,
+        xOrigin: 'left',
+        yOrigin: 'top'
+    });
+    var mCenterCenterAnchor = LINES.createAnchor({
+        el: html.m,
+        xOrigin: 'center',
+        yOrigin: 'center'
+    });
+    var mTopRightAnchor = LINES.createAnchor({
+        el: html.m,
+        xOrigin: 'right',
+        yOrigin: 'top'
+    });
+    _lines.push(LINES.createLine(
+        mTopLeftAnchor
+        ,
+        LINES.createAnchor({
+            el: html.m,
+            xOrigin: 'left',
+            yOrigin: 'bottom'
+        }),
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        mTopLeftAnchor,
+        mCenterCenterAnchor,
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        mCenterCenterAnchor,
+        mTopRightAnchor,
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        mTopRightAnchor,
+        LINES.createAnchor({
+            el: html.m,
+            xOrigin: 'right',
+            yOrigin: 'bottom'
+        }),
+        settings
+    ));
+
+    // L
+    var lLeftBottomAnchor = LINES.createAnchor({
+        el: html.l,
+        xOrigin: 'left',
+        yOrigin: 'bottom'
+    });
+    _lines.push(LINES.createLine(
+        LINES.createAnchor({
+            el: html.l,
+            xOrigin: 'left',
+            yOrigin: 'top'
+        }),
+        lLeftBottomAnchor,
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        lLeftBottomAnchor,
+        LINES.createAnchor({
+            el: html.l,
+            xOrigin: 'right',
+            yOrigin: 'bottom'
+        }),
+        settings
+    ));
+
+    // L
+    var lLeftBottomAnchor2 = LINES.createAnchor({
+        el: lines.l,
+        xOrigin: 'left',
+        yOrigin: 'bottom'
+    });
+    _lines.push(LINES.createLine(
+        LINES.createAnchor({
+            el: lines.l,
+            xOrigin: 'left',
+            yOrigin: 'top'
+        }),
+        lLeftBottomAnchor2,
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        lLeftBottomAnchor2,
+        LINES.createAnchor({
+            el: lines.l,
+            xOrigin: 'right',
+            yOrigin: 'bottom'
+        }),
+        settings
+    ));
+
+    // I
+    _lines.push(LINES.createLine(
+        LINES.createAnchor({
+            el: lines.i,
+            xOrigin: 'center',
+            yOrigin: 'top'
+        }),
+        LINES.createAnchor({
+            el: lines.i,
+            xOrigin: 'center',
+            yOrigin: 'bottom'
+        }),
+        settings
+    ));
+
+    // N
+    var nLeftTopAnchor = LINES.createAnchor({
+        el: lines.n,
+        xOrigin: 'left',
+        yOrigin: 'top'
+    });
+    var nRightBottomAnchor = LINES.createAnchor({
+        el: lines.n,
+        xOrigin: 'right',
+        yOrigin: 'bottom'
+    });
+    _lines.push(LINES.createLine(
+        nLeftTopAnchor,
+        LINES.createAnchor({
+            el: lines.n,
+            xOrigin: 'left',
+            yOrigin: 'bottom'
+        }),
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        nLeftTopAnchor,
+        nRightBottomAnchor,
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        LINES.createAnchor({
+            el: lines.n,
+            xOrigin: 'right',
+            yOrigin: 'top'
+        }),
+        nRightBottomAnchor,
+        settings
+    ));
+
+    // E
+    var eLeftTopAnchor = LINES.createAnchor({
+        el: lines.e,
+        xOrigin: 'left',
+        yOrigin: 'top'
+    });
+    var eLeftBottomAnchor = LINES.createAnchor({
+        el: lines.e,
+        xOrigin: 'left',
+        yOrigin: 'bottom'
+    });
+    _lines.push(LINES.createLine(
+        eLeftTopAnchor,
+        eLeftBottomAnchor,
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        eLeftTopAnchor,
+        LINES.createAnchor({
+            el: lines.e,
+            xOrigin: 'right',
+            yOrigin: 'top'
+        }),
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        LINES.createAnchor({
+            el: lines.e,
+            xOrigin: 'left',
+            yOrigin: 'center'
+        }),
+        LINES.createAnchor({
+            el: lines.e,
+            xOrigin: 'right',
+            yOrigin: 'center'
+        }),
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        eLeftBottomAnchor,
+        LINES.createAnchor({
+            el: lines.e,
+            xOrigin: 'right',
+            yOrigin: 'bottom'
+        }),
+        settings
+    ));
+
+    // S
+    var sLeftAnchor = LINES.createAnchor({
+        el: lines.s,
+        xOrigin: 'left',
+        yOrigin: 0.33333
+    });
+    var sRightAnchor = LINES.createAnchor({
+        el: lines.s,
+        xOrigin: 'right',
+        yOrigin: 0.66667
+    });
+    _lines.push(LINES.createLine(
+        LINES.createAnchor({
+            el: lines.s,
+            xOrigin: 'right',
+            yOrigin: 'top'
+        }),
+        sLeftAnchor,
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        sLeftAnchor,
+        sRightAnchor,
+        settings
+    ));
+    _lines.push(LINES.createLine(
+        sRightAnchor,
+        LINES.createAnchor({
+            el: lines.s,
+            xOrigin: 'left',
+            yOrigin: 'bottom'
+        }),
+        settings
+    ));
+
+    // bind events
+    window.addEventListener('resize', function () {
+        LINES.redraw();
+    });
+}());
